441ad3b62a4eb4906d18a60cda4907ea74ce4fe7
satyajit-dey-17 <satyajit.dey@umbc.edu>
2026-06-06 10:39:55 -0400

    style: black format notifiers/__init__.py

--- drifty/notifiers/__init__.py
+++ drifty/notifiers/__init__.py
@@ -11,8 +11,10 @@ from drifty.notifiers.slack import notify_slack
 
 __all__ = ["notify_slack"]
 
+
 def get_notifier(name: str, **kwargs):
     from drifty.notifiers.slack import SlackNotifier
+
     registry = {"slack": SlackNotifier}
     if name not in registry:
         raise ValueError(f"Unknown notifier: '{name}'. Available: {list(registry)}")
